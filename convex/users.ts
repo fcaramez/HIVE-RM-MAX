235cc18306f180c12ba47f7832ee8189dbc5bc33
fcaramez <francisco.caramez.duarte@gmail.com>
2025-09-26 02:25:28 +0100

    adds provider

--- convex/users.ts
+++ convex/users.ts
@@ -1,9 +1,9 @@
-import { v } from "convex/values";
-import * as argon2 from "argon2";
-import * as jose from "jose";
+import { v } from 'convex/values'
+import * as argon2 from 'argon2'
+import * as jose from 'jose'
 
-import { mutation } from "./_generated/server";
-import { Id } from "./_generated/dataModel";
+import { mutation } from './_generated/server'
+import { Id } from './_generated/dataModel'
 
 export const createUser = mutation({
   args: {
@@ -11,30 +11,30 @@ export const createUser = mutation({
     email: v.string(),
     password: v.string(),
     number: v.number(),
-    type: v.union(v.literal("admin"), v.literal("user")),
+    type: v.union(v.literal('admin'), v.literal('user')),
   },
   handler: async (ctx, args) => {
     try {
-      const { name, email, password, number, type = "user" } = args;
+      const { name, email, password, number, type = 'user' } = args
 
-      const hashedPassword = await argon2.hash(password);
+      const hashedPassword = await argon2.hash(password)
 
-      const user = await ctx.db.insert("users", {
-        id: v.id("users") as unknown as Id<"users">,
+      const user = await ctx.db.insert('users', {
+        id: v.id('users') as unknown as Id<'users'>,
         name,
         email,
         password: hashedPassword,
         number,
         type,
-      });
+      })
 
       if (!user) {
         return {
-          error: "Erro ao criar utilizador",
+          error: 'Erro ao criar utilizador',
           success: false,
-          errorLocation: "global",
-          message: "",
-        };
+          errorLocation: 'global',
+          message: '',
+        }
       }
 
       const payload = {
@@ -43,29 +43,29 @@ export const createUser = mutation({
         email,
         number,
         type,
-      };
+      }
 
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET as string);
-      const algorithm = "HS256";
-      const expiresIn = "30d";
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET as string)
+      const algorithm = 'HS256'
+      const expiresIn = '30d'
 
       const token = await new jose.SignJWT(payload)
         .setProtectedHeader({ alg: algorithm })
         .setExpirationTime(expiresIn)
-        .sign(secret);
+        .sign(secret)
 
       return {
         success: true,
         message: `Bem vindo, ${name}!`,
         token,
-      };
+      }
     } catch (_error) {
       return {
-        error: "Erro inesperado, por favor tente mais tarde",
+        error: 'Erro inesperado, por favor tente mais tarde',
         success: false,
-        errorLocation: "global",
-        message: "",
-      };
+        errorLocation: 'global',
+        message: '',
+      }
     }
   },
-});
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import ConvexClientProvider from './providers/convex-client-provider'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
-  title: "HIVE Studios | RM Max Tracker",
-  description: "A web app made for students and teachers to track their RM Max",
-};
+  title: 'HIVE Studios | RM Max Tracker',
+  description: 'A web app made for students and teachers to track their RM Max',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} antialiased`}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
-  );
+  )
 }
